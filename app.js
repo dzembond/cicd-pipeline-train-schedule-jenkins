fd3fd3ad56d05c0c7d912fac476f16f33e7a77d9
dzembond <43116250+dzembond@users.noreply.github.com>
2019-06-30 12:11:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler additional info
+// error handler additional info fdafdsf
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
